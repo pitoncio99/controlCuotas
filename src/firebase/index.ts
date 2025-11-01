97f79d6bb1b677dd7461bedcd21114357cc37cb6
Victor Marquez <Victor.paez.1clsf@gmail.com>
2025-11-01 08:32:51 +0000

    me dijo esto Automatic initialization failed. Falling back to firebase c

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -18,23 +18,9 @@ const firebaseConfig = {
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
   if (!getApps().length) {
-    // Important! initializeApp() is called without any arguments because Firebase App Hosting
-    // integrates with the initializeApp() function to provide the environment variables needed to
-    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
-    // without arguments.
-    let firebaseApp;
-    try {
-      // Attempt to initialize via Firebase App Hosting environment variables
-      firebaseApp = initializeApp();
-    } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
-
+    // When deploying to a third-party host like Render, we must provide
+    // the configuration object to initializeApp.
+    const firebaseApp = initializeApp(firebaseConfig);
     return getSdks(firebaseApp);
   }
 
@@ -55,6 +41,7 @@ export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
 export * from './non-blocking-updates';
+export * from './auth/use-user';
 export * from './non-blocking-login';
 export * from './errors';
 export * from './error-emitter';
